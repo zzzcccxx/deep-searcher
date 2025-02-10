88cce68a8a2cbae53f89a429c82ecf19b5371952
Ryan Lin <linjinhong@yandex.com>
2025-02-10 12:29:03 -0500

    crawl4ai implemented

--- deepsearcher/loader/web_crawler/__init__.py
+++ deepsearcher/loader/web_crawler/__init__.py
@@ -1,8 +1,10 @@
 from deepsearcher.loader.web_crawler.firecrawl_crawler import FireCrawlCrawler
 from deepsearcher.loader.web_crawler.jina_crawler import JinaCrawler
+from deepsearcher.loader.web_crawler.crawl4ai_crawler import Crawl4AICrawler
 
 
 __all__ = [
     "FireCrawlCrawler",
     "JinaCrawler",
+    "Crawl4AICrawler",
 ]
