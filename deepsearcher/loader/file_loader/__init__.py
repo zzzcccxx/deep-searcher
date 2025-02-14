9f93a559a0f5d3b92ad77464385f9751cb01ce57
Ryan Lin <linjinhong@yandex.com>
2025-02-14 00:53:56 -0500

    refined unstructured loader to implement single file loading

--- deepsearcher/loader/file_loader/__init__.py
+++ deepsearcher/loader/file_loader/__init__.py
@@ -1,6 +1,6 @@
 from deepsearcher.loader.file_loader.pdf_loader import PDFLoader
 from deepsearcher.loader.file_loader.text_loader import TextLoader
-from deepsearcher.loader.file_loader.unstructured import UnstructuredLoader
+from deepsearcher.loader.file_loader.unstructured_loader import UnstructuredLoader
 
 __all__ = [
     "PDFLoader",
