5791de39b553f7c06d452907f5215227107b3f97
Cheney Zhang <chen.zhang@zilliz.com>
2025-02-14 16:25:02 +0800

    fix __init__ file issue
    Signed-off-by: ChengZi <chen.zhang@zilliz.com>

--- deepsearcher/loader/file_loader/__init__.py
+++ deepsearcher/loader/file_loader/__init__.py
@@ -1,6 +1,5 @@
 from deepsearcher.loader.file_loader.pdf_loader import PDFLoader
 from deepsearcher.loader.file_loader.text_loader import TextLoader
-from deepsearcher.loader.file_loader.unstructured import UnstructuredLoader
 from deepsearcher.loader.file_loader.json_loader import JsonFileLoader
 from deepsearcher.loader.file_loader.unstructured_loader import UnstructuredLoader
 
