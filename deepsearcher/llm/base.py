31b1733ad7b4c5512ffbb0202c7352511c877263
Cheney Zhang <chen.zhang@zilliz.com>
2025-02-17 20:01:24 +0800

    robust the llm response parsing function (#28)
    Signed-off-by: ChengZi <chen.zhang@zilliz.com>

--- deepsearcher/llm/base.py
+++ deepsearcher/llm/base.py
@@ -1,4 +1,5 @@
 import ast
+import re
 from abc import ABC
 from typing import Dict, List
 
@@ -20,16 +21,32 @@ class BaseLLM(ABC):
     @staticmethod
     def literal_eval(response_content: str):
         response_content = response_content.strip()
-        if response_content.startswith("```") and response_content.endswith("```"):
-            if response_content.startswith("```python"):
-                response_content = response_content[9:-3]
-            elif response_content.startswith("```json"):
-                response_content = response_content[7:-3]
-            elif response_content.startswith("```str"):
-                response_content = response_content[6:-3]
-            elif response_content.startswith("```\n"):
-                response_content = response_content[4:-3]
-            else:
-                response_content = response_content[3:-3]
-        result = ast.literal_eval(response_content)
+
+        # remove content between <think> and </think>, especial for DeepSeek reasoning model
+        if "<think>" and "</think>" in response_content:
+            end_of_think = response_content.find("</think>") + len("</think>")
+            response_content = response_content[end_of_think:]
+
+        try:
+            if response_content.startswith("```") and response_content.endswith("```"):
+                if response_content.startswith("```python"):
+                    response_content = response_content[9:-3]
+                elif response_content.startswith("```json"):
+                    response_content = response_content[7:-3]
+                elif response_content.startswith("```str"):
+                    response_content = response_content[6:-3]
+                elif response_content.startswith("```\n"):
+                    response_content = response_content[4:-3]
+                else:
+                    raise ValueError("Invalid code block format")
+            result = ast.literal_eval(response_content.strip())
+        except:
+            matches = re.findall(r'(\[.*?\]|\{.*?\})', response_content, re.DOTALL)
+
+            if len(matches) != 1:
+                raise ValueError(f"Invalid JSON/List format for response content:\n{response_content}")
+
+            json_part = matches[0]
+            return ast.literal_eval(json_part)
+
         return result
